7ff63164e517d266b6da6aaf8eff2db47acd93d2
kich555 <hyunjjang199582@gmail.com>
2022-10-07 16:38:03 +0900

    Refactoring routes

--- src/Routes.tsx
+++ src/Routes.tsx
@@ -1,4 +1,5 @@
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import { createBrowserRouter, createRoutesFromElements, Route, RouterProvider } from 'react-router-dom';
+import PostDetailLayout from 'pages/PostDetailLayout';
 
 import BlogLayout from './pages/BlogLayout';
 import BlogPostsPage, { loader as blogPostsLoader } from './pages/BlogPosts';
@@ -10,40 +11,55 @@ import PostDetailPage, { loader as blogPostLoader } from './pages/PostDetail';
 import RootLayout from './pages/RootLayout';
 import WelcomePage from './pages/Welcome';
 
-const router = createBrowserRouter([
-  {
-    path: '/',
-    element: <RootLayout />,
-    errorElement: <ErrorPage />,
-    children: [
-      { index: true, element: <WelcomePage /> },
-      {
-        path: '/blog',
-        element: <BlogLayout />,
-        children: [
-          {
-            index: true,
-            element: <DeferredBlogPostsPage />,
-            loader: deferredBlogPostsLoader,
-          },
-          {
-            path: ':id',
-            element: <PostDetailPage />,
-            loader: blogPostLoader,
-          },
-        ],
-      },
-      {
-        path: '/blog/new',
-        element: <NewPostPage />,
-        action: newPostAction,
-      },
-    ],
-  },
-  {
-    path: '/newsletter',
-    action: newsletterAction,
-  },
-]);
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<RootLayout />} errorElement={<ErrorPage />}>
+      <Route index element={<WelcomePage />} />
+      <Route path="/blog" element={<BlogLayout />}>
+        <Route element={<DeferredBlogPostsPage />} loader={deferredBlogPostsLoader}>
+          <Route index element={<PostDetailLayout />} />
+          <Route path=":id" element={<PostDetailPage />} loader={blogPostLoader} />
+        </Route>
+      </Route>
+      <Route path="/blog/new" element={<NewPostPage />} action={newPostAction} />
+    </Route>,
+  ),
+);
 
 export default router;
+
+// const router = createBrowserRouter([
+//   {
+//     path: '/',
+//     element: <RootLayout />,
+//     errorElement: <ErrorPage />,
+//     children: [
+//       { index: true, element: <WelcomePage /> },
+//       {
+//         path: '/blog',
+//         element: <BlogLayout />,
+//         children: [
+//           {
+//             index: true,
+//             element: <DeferredBlogPostsPage />,
+//             loader: deferredBlogPostsLoader,
+//           },
+//           {
+//             path: ':id',
+//             element: <PostDetailPage />,
+//             loader: blogPostLoader,
+//           },
+//         ],
+//       },
+//       {
+//         path: '/blog/new',
+//         element: <NewPostPage />,
+//         action: newPostAction,
+//       },
+//     ],
+//   },
+//   {
+//     path: '/newsletter',
+//     action: newsletterAction,
+//   },
+// ]);
