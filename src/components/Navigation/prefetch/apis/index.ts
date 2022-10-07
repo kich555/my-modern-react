414305c0fbf30b95560d2e0f98760b794084e1a2
kich555 <hyunjjang199582@gmail.com>
2022-10-07 18:55:43 +0900

    refactoring prefetch todos

--- src/components/Navigation/prefetch/apis/index.ts
+++ src/components/Navigation/prefetch/apis/index.ts
@@ -4,9 +4,7 @@ import request from 'apis/request';
 import { queryClient } from 'index';
 const { posts, post, createPost } = QUERY_KEY;
 
-
-export const prefetchTodos = async () =>
-  // The results of this query will be cached like a normal query
-  await queryClient.prefetchQuery([posts], () => request({ url: '/posts' }));
+// The results of this query will be cached like a normal query
+export const prefetchTodos = async () => await queryClient.prefetchQuery([posts], () => request({ url: '/posts' }));
 
 export const prefetchTodo = async (id: string) => await queryClient.prefetchQuery([post, id], () => getPost(id));
