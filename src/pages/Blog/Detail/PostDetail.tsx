45c190818a132881afecd17e799c9fdfcde3aa7a
kich555 <hyunjjang199582@gmail.com>
2022-10-22 00:13:29 +0900

    [Migration to Ts]

---
+++ src/pages/Blog/Detail/PostDetail.tsx
@@ -0,0 +1,31 @@
+import { Container } from '@mantine/core';
+import { useParams } from 'react-router-dom';
+import { lazy } from 'react';
+import invariant from 'tiny-invariant';
+import AsyncBoundary from 'components/controller/AsyncBoundary';
+import PostsSkeleton from '../List/AsyncHandler/PostSkeleton';
+import PostsErrorHandler from '../List/AsyncHandler/PostsErrorFallback';
+import DefaultPostDetailPage from './DefaultPostDetail';
+import { useSinglePost } from '../apis';
+
+const BlogPost = lazy(() => import('pages/Blog/Detail/components/BlogPost'));
+
+function PostDetailPage() {
+  const id = useParams().id;
+  invariant(id, 'id is required');
+
+  const { title, body } = useSinglePost(id);
+
+  if (!id) {
+    return <DefaultPostDetailPage />;
+  }
+  return (
+    <Container>
+      <AsyncBoundary pendingFallback={<PostsSkeleton />} rejectFallback={PostsErrorHandler}>
+        <BlogPost title={title} body={body} />
+      </AsyncBoundary>
+    </Container>
+  );
+}
+
+export default PostDetailPage;
